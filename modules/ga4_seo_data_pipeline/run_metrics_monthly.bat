0f1367e5bc1bf210fa83ec4c4acf45cdc276e0a6
Josmary Andreina Pinto Diaz <josmary_andreina_17@hotmail.com>
2026-08-24 18:58:41 -0400

    Add modules/ga4_seo_data_pipeline/run_metrics_monthly.bat

---
+++ modules/ga4_seo_data_pipeline/run_metrics_monthly.bat
@@ -0,0 +1,8 @@
+@echo off
+echo ==================================================
+echo Instando Extraccion de Metricas Mensuales
+echo ==================================================
+python main.py --mode metrics
+echo ==================================================
+echo Proceso finalizado.
+pause
