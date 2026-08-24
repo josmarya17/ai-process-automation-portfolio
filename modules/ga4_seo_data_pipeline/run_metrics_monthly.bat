e089540ef250d22cb1954d15207142faa9ebc545
Josmary Andreina Pinto Diaz <josmary_andreina_17@hotmail.com>
2026-08-24 19:05:43 -0400

    Delete old file: modules/ga4_seo_data_pipeline/run_metrics_monthly.bat

--- modules/ga4_seo_data_pipeline/run_metrics_monthly.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ==================================================
-echo Instando Extraccion de Metricas Mensuales
-echo ==================================================
-python main.py --mode metrics
-echo ==================================================
-echo Proceso finalizado.
-pause
